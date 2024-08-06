92a1701984e08d4e1a2a5cdf4c493ba555179d6f
ThiagoB36 <97063776+ThiagoB36@users.noreply.github.com>
2024-08-06 13:55:40 -0400

    Update update-firebase.js

--- src/update-firebase.js
+++ src/update-firebase.js
@@ -1,11 +1,19 @@
-const { initializeApp, applicationDefault, cert } = require('firebase-admin/app');
-const { getFirestore } = require('firebase-admin/firestore');
-const serviceAccount = require('./firebase-admin-sdk.json');
+const { initializeApp } = require('firebase/app');
+const { getFirestore, doc, updateDoc } = require('firebase/firestore');
 
-// Inicialize o Firebase Admin SDK
-initializeApp({
-  credential: cert(serviceAccount)
-});
+// Configuração do Firebase
+const firebaseConfig = {
+  apiKey: "AIzaSyCaZlTO1nedyM_hDmbFHEkQ0N8bQ9JkWLw",
+  authDomain: "flaxboll.firebaseapp.com",
+  projectId: "flaxboll",
+  storageBucket: "flaxboll.appspot.com",
+  messagingSenderId: "707022923688",
+  appId: "1:707022923688:web:62d8bd5e16f18da3d69f9d",
+  measurementId: "G-FEXXR0BBNQ"
+};
+
+// Inicialize o Firebase
+initializeApp(firebaseConfig);
 
 // Obtenha a referência do Firestore
 const db = getFirestore();
@@ -18,8 +26,8 @@ const updateQrCode = async () => {
   console.log({ userId });
   console.log({ projectId });
 
-  const docRef = db.collection('users').doc(userId).collection('projects').doc(projectId);
-  await docRef.update({
+  const docRef = doc(db, 'users', userId, 'projects', projectId);
+  await updateDoc(docRef, {
     qrcodeUrl: qrCodeUrl
   });
 
